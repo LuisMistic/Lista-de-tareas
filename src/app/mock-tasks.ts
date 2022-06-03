e609dda042c8ef7e96999467d1a0d26e76add997
Luis Martinez <luis.escalada@hotmail.com>
2022-06-03 13:22:53 -0300

    mock de tareas

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,31 @@
+import {Task} from './Task'
+
+export const TASKS: Task[] = [
+  {
+      id: 1,
+      text: "terminar primer modulo de Angular",
+      day: 'Agosto 5 a las 12:00',
+      reminder:true,
+  },
+   {
+      id: 2,
+      text: "Hacer las compras para la cena",
+      day: 'Agosto 5 a las 17:00',
+      reminder:true,
+  },
+   {
+      id: 3,
+      text: "Investigar sobre bootstrap",
+      day: 'Agosto 5 a las 19:00',
+      reminder:false,
+  },
+ {
+      id: 4,
+      text: "leer mi libro",
+      day: 'Agosto 5 a las 21:00',
+      reminder:false,
+  },
+
+
+
+]
